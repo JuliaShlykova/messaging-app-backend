1c56aaabb3fe566a028f7156f01bc9331daa8cef
Julia Shlykova <julia.shlykova17@gmail.com>
2024-02-09 15:40:45 +0700

    cors update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ require('./configs/passport.config')(passport);
 const app = express();
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL],
+  origin: [process.env.CLIENT_URL, "http://localhost:5000"],
   credentials: true
 }));
 app.use(helmet());
