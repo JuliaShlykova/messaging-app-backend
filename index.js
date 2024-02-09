dc9f5ea30cb31da2610d67edb4f0e6f6bcc4f3a1
Julia Shlykova <julia.shlykova17@gmail.com>
2024-02-09 15:45:01 +0700

    cors update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ require('./configs/passport.config')(passport);
 const app = express();
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL, "http://localhost:5000"],
+  origin: ["https://messaging-app-frontend-two.vercel.app", "http://localhost:5000"],
   credentials: true
 }));
 app.use(helmet());
